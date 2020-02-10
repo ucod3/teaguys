9a326d4804c04a5a1236f356b8e7b018f60a543d
Aarsh Soni <aarshsoni28@gmail.com>
2020-02-10 10:01:00 -0500

    Header and navigation bar commented

--- themes/teaguys/header.php
+++ themes/teaguys/header.php
@@ -23,7 +23,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'a-theme-just-for-teas' ); ?></a>
-
+<!-- logo for the header-->
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
