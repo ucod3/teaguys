4d2ab1e67c7f563e4b0e214a7eede5b447496986
Usman Butt <usman.butt@dcmail.ca>
2020-02-10 20:43:05 -0500

    deleted skip-link-focus-fix.js reference from functions.php

--- themes/teaguys/functions.php
+++ themes/teaguys/functions.php
@@ -122,8 +122,6 @@ add_action( 'widgets_init', 'a_theme_just_for_teas_widgets_init' );
 function a_theme_just_for_teas_scripts() {
 	wp_enqueue_style( 'tea-guys-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'tea-guys-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
