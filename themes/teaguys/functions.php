c1bdb5f1145fd43b559346bd597765e580e35deb
Usman Butt <usman.butt@dcmail.ca>
2020-02-10 21:12:36 -0500

    enqueue what-foundation.min.js in functions.php

--- themes/teaguys/functions.php
+++ themes/teaguys/functions.php
@@ -122,9 +122,11 @@ add_action( 'widgets_init', 'tea_guys_widgets_init' );
 function tea_guys_scripts() {
 	wp_enqueue_style( 'tea_guys-style', get_stylesheet_uri() );
 
-	wp_enqueue_style( 'tea-guys-foundation', get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', null,'6.5.1' );
+	wp_enqueue_style( 'tea_guys-foundation', get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', null,'6.5.1' );
 
-	wp_enqueue_script( 'tea-guys-what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.js', array('jquery'), '6.5.1', true);
+	wp_enqueue_script( 'tea_guys-what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.js', array('jquery'), '6.5.1', true);
+
+	wp_enqueue_script( 'tea_guys-foundation', get_template_directory_uri() . '/assets/js/vendor/foundation.min.js', array('jquery', 'tea_guys-what-input'), '6.5.1', true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
