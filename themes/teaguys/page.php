2a561f5ac5bb1a41e1e96fffefbd349cd59db586
Dhruvil Modi <dhruvilmodi555@gmail.com>
2020-02-11 00:01:03 -0500

    Added grid-container support to the all pages

--- themes/teaguys/page.php
+++ themes/teaguys/page.php
@@ -22,7 +22,13 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			?>
+
+			<div class="grid-container">
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			</div>
+
+			<?php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
