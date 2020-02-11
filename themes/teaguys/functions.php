b6c3cedddc96365c296e7c0944cca3747b92ae69
Usman Butt <usman.butt@dcmail.ca>
2020-02-10 21:06:42 -0500

    enqueue what-input.js in functions.php

--- themes/teaguys/functions.php
+++ themes/teaguys/functions.php
@@ -124,6 +124,8 @@ function tea_guys_scripts() {
 
 	wp_enqueue_style( 'tea-guys-foundation', get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', null,'6.5.1' );
 
+	wp_enqueue_script( 'tea-guys-what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.js', array('jquery'), '6.5.1', true);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
