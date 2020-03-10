04d023996d84fddccb20fa7839eb6ebbc53f0ab0
Usman Butt <usman.butt@dcmail.ca>
2020-03-10 02:31:21 -0400

    moving if statement for tw out of customizer

--- themes/teaguys/inc/customizer.php
+++ themes/teaguys/inc/customizer.php
@@ -118,15 +118,3 @@ function tea_guys_customize_preview_js() {
 
 add_action( 'customize_preview_init', 'tea_guys_customize_preview_js' );
 
-
-if (get_theme_mod('twitter_url')) {
-	?>
-	<ul class="social-media">
-		<?php if(get_theme_mod('twitter_url')) { ?>
-			<li class="twitter">
-				<a href="<?php echo get_theme_mod('twitter_url'); ?>"><img class="social-icons" src="<?php echo get_template_directory_uri() . '/assets/img/twitter.png'; ?>" alt="<?php echo esc_html__('Twitter', 'tea_guys') ?>"></a>
-			</li>
-		<?php } ?>
-	</ul>
-	<?php
-}
