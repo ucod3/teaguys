78758615874ad884b61e68f519040a27898c2815
Usman Butt <usman.butt@dcmail.ca>
2020-03-09 22:38:11 -0400

    actually more error cames when previewing the customizer, then notice everything needed to be inside  tea_guys_customize_register(){}

--- themes/teaguys/inc/customizer.php
+++ themes/teaguys/inc/customizer.php
@@ -25,39 +25,12 @@ function tea_guys_customize_register( $wp_customize ) {
 			'render_callback' => 'tea_guys_customize_partial_blogdescription',
 		) );
 	}
-}
-add_action( 'customize_register', 'tea_guys_customize_register' );
-
-/**
- * Render the site title for the selective refresh partial.
- *
- * @return void
- */
-function tea_guys_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-/**
- * Render the site tagline for the selective refresh partial.
- *
- * @return void
- */
-function tea_guys_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function tea_guys_customize_preview_js() {
-	wp_enqueue_script( 'tea_guys-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
-
 
 /**
  * panels
  */
 $wp_customize -> add_panel('social_media_panel', array(
-	'title' => exc_html__('Social Media', 'tea_guys'),
+	'title' => esc_html__('Social Media', 'tea_guys'),
 	'capability' => 'edit_theme_options'
 ));
 
@@ -115,6 +88,34 @@ $wp_customize -> add_control('twitter_url', array(
 ));
 
 }
+add_action( 'customize_register', 'tea_guys_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function tea_guys_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function tea_guys_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function tea_guys_customize_preview_js() {
+	wp_enqueue_script( 'tea_guys-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+
+}
+
 add_action( 'customize_preview_init', 'tea_guys_customize_preview_js' );
 
 
