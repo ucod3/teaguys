55b82d9f1e68e4fbc3060b29207fd80a2995d466
Aarsh Soni <aarshsoni28@gmail.com>
2020-03-24 21:44:33 -0400

    adding while loop to the single page template to get the custom post type with heading

--- themes/teaguys/template-news.php
+++ themes/teaguys/template-news.php
@@ -4,6 +4,11 @@ get_header();
 query_posts(array(
 'post_type' => 'news'
 )); ?>
-<?php
 
+<?php
+    while (have_posts()) : the_post(); ?>
+    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_excerpt(); ?></p>
+    <?php endwhile;
+get_footer();
 ?>
