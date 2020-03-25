2fc75d05680dcc4a5227783a68e3c2d6c0617967
Aarsh Soni <aarshsoni28@gmail.com>
2020-03-24 21:48:31 -0400

    Fetching the data to see the detail page

---
+++ themes/teaguys/single-news.php
@@ -0,0 +1,8 @@
+<?php
+get_header();
+/* Start the Loop */
+while (have_posts()) : the_post();
+get_template_part('template-parts/post/content', get_post_format());
+endwhile; // End of the loop.
+get_footer();
+?>
