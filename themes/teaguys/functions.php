eddfded17e3fde8bcd83396a3a3d3dd92be775ee
Aarsh Soni <aarshsoni28@gmail.com>
2020-03-24 21:22:40 -0400

    Hooking up our function with theme script

--- themes/teaguys/functions.php
+++ themes/teaguys/functions.php
@@ -188,12 +188,14 @@ function create_posttype() {
 	// CPT Options
 	array(
 		'labels' => array(
-		 'name' => __( 'news' ),
-		 'singular_name' => __( 'News' )
+		'name' => __( 'news' ),
+		'singular_name' => __( 'News' )
 		),
 		'public' => true,
 		'has_archive' => false,
 		'rewrite' => array('slug' => 'news'),
-	   )
-	  );
-	}	  
+	)
+	);
+	}
+	// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );	  
