2a7cba852d8f417ab0224bd983e32a953b06672d
Dhruvil Modi <dhruvilmodi555@gmail.com>
2020-03-30 18:47:26 -0400

    comment in line 16

--- themes/teaguys/assets/js/main.js
+++ themes/teaguys/assets/js/main.js
@@ -13,7 +13,8 @@ $(document).ready(function(){
     gsap.to(".welcomePage",1,{opacity:0, delay:2});
     gsap.to(".welcomePage",1,{display:"none", delay:2.5});
     
-    gsap.to("#logoImgLoading",0.2,{y:-10, repeat:-1, yoyo: true});
+    // logo image animation
+    gsap.to("#logoImgLoading",0.2,{y:-10, repeat:5, yoyo: true});
 
     gsap.to(".welcomePageOther",1,{opacity:0, delay:1});
     gsap.to(".welcomePageOther",1,{display:"none", delay:1.5});
